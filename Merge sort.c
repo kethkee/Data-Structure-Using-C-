92f161dab9df1e8f0230e080b776e8867a0752c3
Keerthana P B <166291742+kethkee@users.noreply.github.com>
2024-10-24 12:45:13 +0530

    Update Merge sort.c

--- Merge sort.c
+++ Merge sort.c
@@ -1,77 +1,90 @@
 #include <stdio.h>
-
-// Function to merge two subarrays
-void merge(int arr[], int left, int mid, int right) {
+void merge(int arr[], int left, int mid, int right) 
+{
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
-    // Create temp arrays
     int leftArr[n1], rightArr[n2];
 
-    // Copy data to temp arrays
+    
     for (int i = 0; i < n1; i++)
+    {
         leftArr[i] = arr[left + i];
+    } 
     for (int i = 0; i < n2; i++)
+    {
+    
         rightArr[i] = arr[mid + 1 + i];
+    }   
 
-    // Merge the temp arrays back into arr[l..r]
+    
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (leftArr[i] <= rightArr[j]) {
+    while (i < n1 && j < n2) 
+    {
+        if (leftArr[i] <= rightArr[j]) 
+        {
             arr[k] = leftArr[i];
             i++;
-        } else {
+        } else 
+        {
             arr[k] = rightArr[j];
             j++;
         }
         k++;
     }
 
-    // Copy the remaining elements of leftArr[], if any
-    while (i < n1) {
+    
+    while (i < n1) 
+    {
         arr[k] = leftArr[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of rightArr[], if any
-    while (j < n2) {
+    
+    while (j < n2) 
+    {
         arr[k] = rightArr[j];
         j++;
         k++;
     }
 }
 
-// Function to implement merge sort
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
+
+void mergeSort(int arr[], int left, int right) 
+{
+    if (left < right) 
+    {
         int mid = left + (right - left) / 2;
-
-        // Recursively sort first and second halves
-        mergeSort(arr, left, mid);
+	mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
-
-        // Merge the sorted halves
-        merge(arr, left, mid, right);
+	merge(arr, left, mid, right);
     }
 }
 
-// Function to print the array
-void printArray(int arr[], int size) {
+
+void printArray(int arr[], int size) 
+{
     for (int i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
+    }    
     printf("\n");
 }
 
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
-
+int main() 
+{
+    int arr_size,i;
+    printf("Enter the number of terms in the array :");
+    scanf("%d",&arr_size);
+    int arr[arr_size];
+    printf("Enter array elements:");
+    for(i = 0;i < arr_size;i++)
+    {
+    	scanf("%d",&arr[i]);
+    }	
     printf("Given array is \n");
     printArray(arr, arr_size);
-
     mergeSort(arr, 0, arr_size - 1);
-
     printf("\nSorted array is \n");
     printArray(arr, arr_size);
     return 0;
